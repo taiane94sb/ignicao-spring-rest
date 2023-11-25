1f82ee370969b24f05f388a09c4fc902b53d3ca6
taiane94sb <taiane19sb94@gmail.com>
2023-11-25 12:42:20 -0300

    first commit

---
+++ src/test/java/com/taianesb/transito/TransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taianesb.transito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
