c7c0a2fff27fcb7b0cb5a91eb4f6a06582afc1b7
taiane94sb <taiane19sb94@gmail.com>
2023-11-29 19:58:01 -0300

    implementando endpoint de inclusão de recurso

--- src/main/java/com/taianesb/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/taianesb/transito/api/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package com.taianesb.transito.api.controller;
 import com.taianesb.transito.domain.model.Proprietario;
 import com.taianesb.transito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,12 +26,12 @@ public class ProprietarioController {
         return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
 
-//        if (proprietario.isPresent()) {
-//            return ResponseEntity.ok(proprietario.get());
-//        }
-//
-//        return ResponseEntity.notFound().build();
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 
 }
