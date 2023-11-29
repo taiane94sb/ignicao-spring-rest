50f218b147ddae781b05bff48853409f416e0eaa
taiane94sb <taiane19sb94@gmail.com>
2023-11-29 15:53:15 -0300

    mapeando entidades com jakarta persistence

--- src/main/java/com/taianesb/transito/domain/model/Proprietario.java
+++ src/main/java/com/taianesb/transito/domain/model/Proprietario.java
@@ -1,15 +1,27 @@
 package com.taianesb.transito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+//@Table(name = "tb_proprietario")
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
 }
