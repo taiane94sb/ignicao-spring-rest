c79016f17333fd3dbda9bcd7ea4ace0133de05a4
taiane94sb <taiane19sb94@gmail.com>
2023-11-29 20:10:04 -0300

    implementando endpoint de atualização de recurso

--- src/main/java/com/taianesb/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/taianesb/transito/api/controller/ProprietarioController.java
@@ -34,4 +34,17 @@ public class ProprietarioController {
         return proprietarioRepository.save(proprietario);
     }
 
+    @PutMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId,
+                                                  @RequestBody Proprietario proprietario) {
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+           return ResponseEntity.notFound().build();
+        }
+
+        proprietario.setId(proprietarioId);
+        Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
+
+        return ResponseEntity.ok(proprietarioAtualizado);
+    }
+
 }
