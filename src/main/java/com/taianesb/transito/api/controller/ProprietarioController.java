4eccb6a4362149930efdd2891d80f86d00623ff6
taiane94sb <taiane19sb94@gmail.com>
2023-11-29 17:14:48 -0300

    injetando e usando o repositório do sdj - construtor

--- src/main/java/com/taianesb/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/taianesb/transito/api/controller/ProprietarioController.java
@@ -2,9 +2,6 @@ package com.taianesb.transito.api.controller;
 
 import com.taianesb.transito.domain.model.Proprietario;
 import com.taianesb.transito.domain.repository.ProprietarioRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +10,12 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
-    @Autowired
-    private ProprietarioRepository proprietarioRepository;
+    private final ProprietarioRepository proprietarioRepository;
+
+    public ProprietarioController(ProprietarioRepository proprietarioRepository) {
+        this.proprietarioRepository = proprietarioRepository;
+    }
+
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
