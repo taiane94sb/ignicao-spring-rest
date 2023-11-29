8d60da208ab21e9805678bffd3fbc55fc0875fb0
taiane94sb <taiane19sb94@gmail.com>
2023-11-29 17:16:42 -0300

    injetando e usando o repositório do sdj - @AllArgsConstructor

--- src/main/java/com/taianesb/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/taianesb/transito/api/controller/ProprietarioController.java
@@ -2,20 +2,17 @@ package com.taianesb.transito.api.controller;
 
 import com.taianesb.transito.domain.model.Proprietario;
 import com.taianesb.transito.domain.repository.ProprietarioRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
 
     private final ProprietarioRepository proprietarioRepository;
-
-    public ProprietarioController(ProprietarioRepository proprietarioRepository) {
-        this.proprietarioRepository = proprietarioRepository;
-    }
-
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
