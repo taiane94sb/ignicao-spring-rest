804c916d2e8f8a8f602f307cb60a0fadfed9ea2f
taiane94sb <taiane19sb94@gmail.com>
2023-11-30 09:28:52 -0300

    implementando regra de negócio para restringir e-mails duplicados

--- src/main/java/com/taianesb/transito/domain/service/RegistroProprietarioService.java
+++ src/main/java/com/taianesb/transito/domain/service/RegistroProprietarioService.java
@@ -1,5 +1,6 @@
 package com.taianesb.transito.domain.service;
 
+import com.taianesb.transito.domain.exception.NegocioException;
 import com.taianesb.transito.domain.model.Proprietario;
 import com.taianesb.transito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
@@ -14,6 +15,15 @@ public class RegistroProprietarioService {
 
     @Transactional
     public Proprietario salvar(Proprietario proprietario) {
+
+        boolean emailEmUso = proprietarioRepository.findByEmail(proprietario.getEmail())
+                .filter(p -> !p.equals(proprietario))
+                .isPresent();
+
+        if (emailEmUso) {
+            throw new NegocioException("Já existe um proprietário cadastrado com este e-mail");
+        }
+
         return proprietarioRepository.save(proprietario);
     }
 
