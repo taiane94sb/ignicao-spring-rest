35aaca18a256f6768a997c77e64d2690a86c3048
taiane94sb <taiane19sb94@gmail.com>
2023-12-05 20:15:57 -0300

    boas práticas para trabalhar com data/hora

--- src/main/java/com/taianesb/transito/domain/service/RegistroVeiculoService.java
+++ src/main/java/com/taianesb/transito/domain/service/RegistroVeiculoService.java
@@ -9,7 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @AllArgsConstructor
 @Service
@@ -36,7 +36,7 @@ public class RegistroVeiculoService {
 
         veiculo.setProprietario(proprietario);
         veiculo.setStatus(StatusVeiculo.REGULAR);
-        veiculo.setDataCadastro(LocalDateTime.now());
+        veiculo.setDataCadastro(OffsetDateTime.now());
 
         return veiculoRepository.save(veiculo);
     }
