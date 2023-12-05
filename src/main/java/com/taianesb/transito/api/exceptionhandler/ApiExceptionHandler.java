7ad32531de698bfb210ec87b1457bc37b527d46d
taiane94sb <taiane19sb94@gmail.com>
2023-12-05 15:28:09 -0300

    capturando exceções globais com @RestControllerAdvice

---
+++ src/main/java/com/taianesb/transito/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.taianesb.transito.api.exceptionhandler;
+
+import com.taianesb.transito.domain.exception.NegocioException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<String> capturar(NegocioException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
