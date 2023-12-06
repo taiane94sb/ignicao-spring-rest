8e190fe0f4d1584689a00ecaa6921b31eccd9da1
taiane94sb <taiane19sb94@gmail.com>
2023-12-06 14:58:25 -0300

    compondo objetos no Representation Model

---
+++ src/main/java/com/taianesb/transito/api/model/ProprietarioResumoModel.java
@@ -0,0 +1,12 @@
+package com.taianesb.transito.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProprietarioResumoModel {
+
+    private Long id;
+    private String nome;
+}
