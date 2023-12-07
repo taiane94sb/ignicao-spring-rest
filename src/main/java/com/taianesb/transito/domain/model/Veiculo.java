7de655384da1f774e950fbcb98bfa6c67f9fd4ce
taiane94sb <taiane19sb94@gmail.com>
2023-12-07 20:12:18 -0300

    implementando regras de negócio de apreensão de veículo

--- src/main/java/com/taianesb/transito/domain/model/Veiculo.java
+++ src/main/java/com/taianesb/transito/domain/model/Veiculo.java
@@ -1,5 +1,6 @@
 package com.taianesb.transito.domain.model;
 
+import com.taianesb.transito.domain.exception.NegocioException;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -44,4 +45,29 @@ public class Veiculo {
         return autuacao;
     }
 
+    public void apreender() {
+        if (estaApreendido()) {
+            throw new NegocioException("Veículo já se encontra apreendido");
+        }
+
+        setStatus(StatusVeiculo.APREENDIDO);
+        setDataApreensao(OffsetDateTime.now());
+    }
+
+    public void removerApreensao() {
+        if (naoEstaApreendido()) {
+            throw new NegocioException("Veículo não está apreendido");
+        }
+
+        setStatus(StatusVeiculo.REGULAR);
+        setDataApreensao(null);
+    }
+
+    public boolean estaApreendido() {
+        return StatusVeiculo.APREENDIDO.equals(getStatus());
+    }
+
+    public boolean naoEstaApreendido() {
+        return !estaApreendido();
+    }
 }
